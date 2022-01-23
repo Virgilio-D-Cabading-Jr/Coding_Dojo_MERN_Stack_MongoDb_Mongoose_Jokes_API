a076caf72da9c3eb4b8291b9dabb3745c6faaf70
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-22 20:05:26 -0800

    implement server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+/////////////////////////////////////////////////
+//  SERVER JAVASCRIPT
+/////////////////////////////////////////////////
+
+// //// FIELDS //////////////////////////////////
 const express = require("express");
 const app = express();
+const PORT = 8000;
+
+// //// MIDDLE-WARE /////////////////////////////
+app.use(express.json(), express.urlencoded({ extended: true }));
 
 // This will fire our mongoose.connect statement to initialize our database connection
 require("./server/config/mongoose.config");
-
-app.use(express.json(), express.urlencoded({ extended: true }));
 
 // This is where we import the users routes function from our user.routes.js file
 const AllMyUserRoutes = require("./server/routes/user.routes");
