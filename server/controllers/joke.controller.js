9992d7a94d6e24785908abb7611220eb4f779d96
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-23 06:16:40 -0800

    add findAllJokes to joke controller

--- server/controllers/joke.controller.js
+++ server/controllers/joke.controller.js
@@ -7,13 +7,44 @@ const Joke = require("../models/joke.model");
 
 // //// CREATE //////////////////////////////////
 
+/**
+ * Create a New Joke
+ * @param {*} req 
+ * @param {*} res 
+ */
 module.exports.createNewJoke = (req, res) => {
     User.create(req.body)
-      .then(newlyCreatedUser => res.json({ joke: newlyCreatedJoke }))
-      .catch(err => res.json({ message: "🤦🤦🏼🤦 Create was not successful 🤦🤦🏼🤦🏼", error: err }));
-  };
+        .then(newlyCreatedUser => res.json({ joke: newlyCreatedJoke }))
+        .catch(err => 
+            res.json({ 
+                message: "🤦🤦🏼🤦 Create was not successful 🤦🤦🏼🤦🏼", 
+                error: err 
+            })
+        );
+    };
 
 // //// RETRIEVE ////////////////////////////////
+
+/***
+ * Find All Jokes
+ * @param (*) req
+ * @param (*) res
+ */
+module.exports.findAllJokes = (req, res) => {
+    Joke.find()
+        .then( allJokes => 
+            res.json({
+                jokes: allJokes,
+                message: "🍟🍟🍟 Success: Found All Jokes 🍟🍟🍟"
+            })
+        )
+        .catch( err =>
+            res.json({
+                message: "🤚🏼🤚🏼🤚🏼 UnAble to Find All Jokes ✋🏼✋🏼✋🏼",
+                error: err
+            })
+        )
+};
 
 // //// UPDATE //////////////////////////////////
 
