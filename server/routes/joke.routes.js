031e84d608510b4df95e58596b9378e5b4552ece
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-23 07:23:34 -0800

    add find allJokes and findJoke routes

--- server/routes/joke.routes.js
+++ server/routes/joke.routes.js
@@ -1,3 +1,22 @@
 /////////////////////////////////////////////////
 //  JOKE ROUTE
 /////////////////////////////////////////////////
+
+// //// FIELDS //////////////////////////////////
+const JokeController = require("../controllers/joke.controller");
+const Joke = require("../models/joke.model");
+
+// //// ROUTES //////////////////////////////////
+module.exports = app => {
+    // **** Create ******************************
+
+    // **** Retrieve ****************************
+    app.get("/api/jokes", JokeController.findAllJokes);
+    app.get("/api/jokes/:id", JokeController.fineOneJoke);
+
+    // **** Update ******************************
+
+    // **** Delete ******************************
+
+
+}
