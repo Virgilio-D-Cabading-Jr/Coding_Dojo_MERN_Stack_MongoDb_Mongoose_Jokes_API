b85cec1e9f1e75ca0ded58d82b4b38fcf26e0edf
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-23 06:33:54 -0800

    add minLength to setup and punchline in Joke Model

--- server/controllers/joke.controller.js
+++ server/controllers/joke.controller.js
@@ -14,9 +14,14 @@ const Joke = require("../models/joke.model");
  */
 module.exports.createNewJoke = (req, res) => {
     User.create(req.body)
-        .then(newlyCreatedUser => res.json({ joke: newlyCreatedJoke }))
-        .catch(err => 
-            res.json({ 
+        .then(
+            newlyCreatedUser => res.json({ 
+                joke: newlyCreatedJoke,
+                message: "🍻🍻🍻 Success: Created a Joke 🍻🍻🍻"
+            })
+        )
+        .catch(
+            err => res.json({ 
                 message: "🤦🤦🏼🤦 Create was not successful 🤦🤦🏼🤦🏼", 
                 error: err 
             })
@@ -40,8 +45,23 @@ module.exports.findAllJokes = (req, res) => {
         )
         .catch( err =>
             res.json({
-                message: "🤚🏼🤚🏼🤚🏼 UnAble to Find All Jokes ✋🏼✋🏼✋🏼",
+                message: "🤚🏼🤚🏼🤚🏼 Failure: UnAble to Find All Jokes ✋🏼✋🏼✋🏼",
                 error: err
+            })
+        )
+};
+
+/**
+ * Find One Joke by ID (on Request Params)
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.fineOneJoke = (req, res) => {
+    Joke.findById(req.params.id)
+        .then(
+            aJoke => res.json ({
+                joke: aJoke,
+                message: " Success: Found a Joke "
             })
         )
 };
--- server/models/joke.model.js
+++ server/models/joke.model.js
@@ -9,11 +9,13 @@ const mongoose = require("mongoose");
 const JokeSchema = new mongoose.Schema({
 	setup: {
 		type: String,
-		required: true
+		required: true,
+		minlength: 10
 	},
 	punchline: {
 		type: String, 
-		required: true
+		required: true,
+		minlength: 3
 	}
 }, {timestamps: true});			// Timestamps implement CreatedAt and UpdatedAt
 
